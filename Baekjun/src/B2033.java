bb4f126776a61111b29a605ba43456b6d688e7bd
이진하 <dkwlsfk2323@gmail.com>
2020-07-09 15:34:23 +0900

    문제 추가

--- Baekjun/src/B2033.java
+++ Baekjun/src/B2033.java
@@ -1,4 +1,19 @@
-
+import java.util.*;
 public class B2033 {
-
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long num = sc.nextInt();
+		int start = 10;
+		while(num > start) {
+			if(num%start >= 5*(start/10) ) {
+				num += (start - num%start);
+				start *= 10;
+			}else {
+				num -= num%start;
+				start *= 10;
+			}
+			
+		}
+		System.out.println(num);
+	}
 }
