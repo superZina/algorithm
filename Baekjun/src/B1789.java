5ae667d8c53a547ad74a8b932d4dda414e687b02
이진하 <dkwlsfk2323@gmail.com>
2020-07-12 22:43:59 +0900

    수정

--- Baekjun/src/B1789.java
+++ Baekjun/src/B1789.java
@@ -2,13 +2,12 @@ import java.util.*;
 public class B1789 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int result = 0;
-		int count = 1; 
+		long n = sc.nextLong();
+		long result = 0;
+		long count = 1; 
 		double disc = 1 + 4.0*n*2.0;
 		double sqr = Math.sqrt(disc);
 		double max = Math.max(-1 + sqr, -1 - sqr);
-		System.out.println(max/2);
-		System.out.println((int)(max/2));
+		System.out.println((long)(max/2));
 	}
-}
+ }
