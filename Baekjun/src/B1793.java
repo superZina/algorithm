a18fef197b18537b324d0ebf64b3e0bdb188e4fa
이진하 <dkwlsfk2323@gmail.com>
2020-08-03 23:28:12 +0900

    문제 추가/수정

--- Baekjun/src/B1793.java
+++ Baekjun/src/B1793.java
@@ -1,10 +1,43 @@
+import java.math.BigInteger;
 import java.util.*;
 public class B1793 {
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
-		while(sc.hasNext()) {
-			int n = sc.nextInt();
-			int dp
+		while(sc.hasNextLine()) {
+			String prob = sc.nextLine();
+//			BigInteger n = sc.nextBigInteger();
+			StringTokenizer st = new StringTokenizer(prob);
+			BigInteger n;
+			if(!prob.isEmpty()) {
+				//string을 bigInteger로
+				n = BigInteger.valueOf(Long.parseLong(st.nextToken()));
+			}else {
+				break;
+			}
+			if(n == BigInteger.ONE || n == BigInteger.valueOf(2)) {
+				int[] d = new int[3];
+				d[0] = 0;
+				d[1] = 1;
+				d[2] = 3;
+				long res = d[n.intValue()]%10007;
+				System.out.println(res);
+			}else {
+				int N = n.intValue();
+			BigInteger[] dp = new BigInteger[N+1];
+			dp[0] = BigInteger.ZERO;
+			dp[1] = BigInteger.ONE;
+			dp[2] = BigInteger.valueOf(3);
+			for(int i=3; i<=N; i++) {
+				BigInteger tmp = dp[i-2].multiply(BigInteger.valueOf(2));
+				dp[i] = dp[i-1].add(tmp) ;
+//				dp[i] = dp[i]%0;
+			}
+			
+			
+			
+			System.out.println(dp[n.intValue()]);
+			}
+//			System.out.print(st.nextToken());
 		}
 		
 	}
---
+++ Baekjun/src/B9251.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class B9251 {
+	public static int LCS(String word1, String word2) {
+		int len = 0;
+		int count =0;
+		for(int i=0; i<word1.length(); i++) {
+			for(int j = count+1; j<=i; j++) {
+				if(word1.charAt(i) == word2.charAt(j)) {
+					count = j;
+					len++;
+//				System.out.println("word1 : "+word1.charAt(i));
+//				System.out.println("word2 : "+word2.charAt(j));
+				}else continue;
+			}
+		}
+		return len;
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String word1 = sc.next();
+		String word2 = sc.next();
+		int len1 = LCS(word1, word2);
+		int len2 = LCS(word2,word1);
+		int lcs = Math.max(len1, len2);
+		System.out.print(lcs+1);
+	}
+}
