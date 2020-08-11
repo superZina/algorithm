c687acc83172ff4c79d64a4fc08e9635a92770c5
이진하 <dkwlsfk2323@gmail.com>
2020-08-11 21:42:45 +0900

    문제 추가 및 수정

--- Baekjun/src/B1213.java
+++ Baekjun/src/B1213.java
@@ -3,55 +3,64 @@ import java.util.*;
 public class B1213 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] name = br.readLine().split("");
-		Arrays.sort(name);
+		String text = br.readLine();
+		String[] name = text.split("");
 		
-		String[] pelin = new String[name.length];
+		
+		
+		//홀수인 알파벳이 2개이상이면 팰린드롬만들지 못함.
+		int[] alph = new int[30];
 		int count = 0;
-		int Idx = 0;
-		boolean isAble = true;
-		String last = "";
-		for(int i=0;i<name.length;i+=2) {
-			if(i<name.length-1 && !name[i].equals(name[i+1])) {
-//				System.out.println("name["+i+"] : "+name[i]+" name["+(i+1)+"] : "+name[i+1]);
-				last = name[i];
-				
-				count++;
-				if(count>1) {
-//					System.out.println("I'm Sorry Hansoo");
-					isAble = false;
-					break;
-				}else {
-					if(i+1 == name.length-1) {
-//						System.out.println("I'm Sorry Hansoo");
-						isAble = false;
-						break;
-					}else {
-						i--;
-						continue;
-					
-					}
-				}
-			}else{
-				pelin[Idx] = name[i];
-				pelin[pelin.length-1-Idx] = name[i];
-				Idx++;
-			}
+		for(int i=0; i< text.length(); i++) {
+			alph[text.charAt(i)-'A']++;
 		}
 		
-//		System.out.println();
-//		System.out.println("last : "+last);
-		for(int i=0;i<name.length ; i++) {
-			if(pelin[i]==null) {
-				pelin[i] = last;
+		for(int i=0; i< text.length(); i++) {
+			if(alph[i]%2==1) {
+				count++;
 			}
 		}
-		if(isAble) {
-			for(int i=0;i<name.length ; i++) {
-				System.out.print(pelin[i]);
-			}
-		}else {
+		if(count>1) {
 			System.out.println("I'm Sorry Hansoo");
+		}
+		Arrays.sort(name);
+		char[] pelin = new char[name.length];
+		
+		int midIdx = 0;
+		
+		int pelinIdx = 0;
+		for(int i=0; i<alph.length; i++) {
+			//알파벳 개수가 짝수일 때
+			//짝수이고 2개이상일 때
+			if(alph[i]%2==0) {
+				int alcount = alph[i]/2;
+				for(int j=0; j<alcount; j++) {
+					pelin[pelinIdx] = (char) (i+'A');
+					pelin[pelin.length-1-pelinIdx] = (char) (i+'A');
+					pelinIdx++;
+				}
+			}else {
+				int alcount = alph[i]/2;
+				for(int j=0; j<alcount; j++) {
+					pelin[pelinIdx] = (char) (i+'A');
+					pelin[pelin.length-1-pelinIdx] = (char) (i+'A');
+					pelinIdx++;
+				}
+				midIdx = i;
+			}
+			// 홀수일 때
+		}
+		
+		for(int i=0;i<name.length ; i++) {
+			if(pelin[i] == '\u0000' ) {
+				pelin[i] = (char) (midIdx+'A');
+			}
+		}
+		if(count<=1) {
+			for(int i=0;i<name.length ; i++) {
+				System.out.print(Character.toString(pelin[i]));
+			}
+			System.out.println();
 		}
 	}
 }
